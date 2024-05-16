506c97e83de853f57a1fed4e72f712361229d6f5
Yuri Cavalcanti <ycs@cesar.school>
2024-05-16 17:48:35 -0300

    codigo inicio

--- Projeto.py
+++ Projeto.py
@@ -8,3 +8,13 @@ class Receita:
 #-----------= Listas =---------------#
 lista_receitas = []
 Receitas_Fav = []
+#----------= Começo do código =-----------#
+while True:
+    print("\nMenu:")
+    print("1. Cadastrar receita")
+    print("2. Visualizar receitas")
+    print("3. Atualizar receita")
+    print("4. Excluir receita")
+    print("5. Favoritar receita")
+    print("6. Visualizar Favoritos")
+    print("7. Sair")
