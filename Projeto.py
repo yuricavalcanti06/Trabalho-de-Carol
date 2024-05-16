5c056096c5b5db38c3dafb2e7b36f40afc5075ec
Yuri Cavalcanti <ycs@cesar.school>
2024-05-16 17:53:05 -0300

    funcoes base

--- Projeto.py
+++ Projeto.py
@@ -18,3 +18,23 @@ while True:
     print("5. Favoritar receita")
     print("6. Visualizar Favoritos")
     print("7. Sair")
+    opcao = input("\nDigite o número da opção desejada: ")
+    if opcao == "1":
+            cadastrar_receita(lista_receitas)
+            elif opcao == "2":
+            lista_receitas = []
+            visualizar_receitas(lista_receitas)
+        elif opcao == "3":
+            lista_receitas = []
+            atualizar_receita(lista_receitas)
+        elif opcao == "4":
+            lista_receitas = []
+            excluir_receita(lista_receitas)
+        elif opcao == "5":
+            favoritar_receita(lista_receitas)
+        elif opcao == "6":
+            Receitas_Fav = []
+            visualizar_favoritos(Receitas_Fav)
+        elif opcao == "7":
+            print("\nEncerrando o programa. Até mais!")
+            break
