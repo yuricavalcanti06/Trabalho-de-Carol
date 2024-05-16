e7e9edb572118bab5085974347cb05654f8f9feb
Yuri Cavalcanti <ycs@cesar.school>
2024-05-16 17:55:07 -0300

    Listas

--- Projeto.py
+++ Projeto.py
@@ -4,3 +4,6 @@ class Receita:
         self.paisdeorigem = ""
         self.ingredientes = ""
         self.mododepreparo = ""
+#-----------= Listas =---------------#
+lista_receitas = []
+Receitas_Fav = []
